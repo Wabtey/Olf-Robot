86169c8a3978427b0ad14b709a8d9f316fbad9c1
= <73140258+Wabtey@users.noreply.github.com>
2022-02-19 04:32:53 +0100

    clean indentation

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,46 @@
-use std::env;
 
-          use serenity::{
-          	async_trait,
-          	model::{channel::Message, gateway::Ready},
-          	prelude::*,
-          };
+mod constants;
 
-          const LIVE_OFF_NELSON: &str = "
-          : OFF :no:  shisOPTI  :Oiso: 
-          
-          twitch game : **twitch title**
-          
-          https://www.twitch.tv/sombre_roi_rene
-          
-          ";
+use std::{
+  env,
+};
 
-          const LIVE_COMMAND: &str = "!live";
+use constants::*;
 
-          struct Handler;
+use serenity::{
+	async_trait,
+  model::{channel::Message, gateway::Ready},
+	prelude::*,
+};
 
-          #[async_trait]
-          impl EventHandler for Handler {
-            async fn message(&self, ctx: Context, msg: Message) {
-              if msg.content == LIVE_COMMAND {
-                if let Err(why) = msg.channel_id.say(&ctx.http, LIVE_OFF_NELSON).await {
-                	println!("Error sending message: {:?}", why);
-              }
-            }
-          }
+struct Handler;
 
-          async fn ready(&self, _: Context, ready: Ready) {
-          	println!("{} is connected!", ready.user.name);
-          	}
-          }
-          #[tokio::main]
-          async fn main() {
-            let token = env::var("DISCORD_TOKEN")
-            .expect("Expected a token in the environment");
+#[async_trait]
+impl EventHandler for Handler {
+  async fn message(&self, ctx: Context, msg: Message) {
+    if msg.content == LIVE_COMMAND {
+      if let Err(why) = msg.channel_id.say(&ctx.http, LIVE_OFF_NELSON).await {
+        println!("Error sending message: {:?}", why);
+      }
+    }
+  }
 
-            let mut client = Client::builder(&token)
-            .event_handler(Handler)
-            .await
-            .expect("Err creating client");
+  async fn ready(&self, _: Context, ready: Ready) {
+    println!("{} is connected!", ready.user.name);
+  }
+}
 
-            if let Err(why) = client.start().await {
-            	println!("Client error: {:?}", why);
-            }
-          }
+#[tokio::main]
+async fn main() {
+  let token = env::var("DISCORD_TOKEN")
+  .expect("Expected a token in the environment");
+
+  let mut client = Client::builder(&token)
+  .event_handler(Handler)
+  .await
+  .expect("Err creating client");
+
+  if let Err(why) = client.start().await {
+    println!("Client error: {:?}", why);
+  }
+}
