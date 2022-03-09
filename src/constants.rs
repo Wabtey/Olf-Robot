67934bc9b2c4a1286ba1cbc307bda90bfa054a37
Wabtey <73140258+Wabtey@users.noreply.github.com>
2022-03-09 13:06:17 +0100

    Elzapat's Explanations

---
+++ src/constants.rs
@@ -0,0 +1,8 @@
+pub const LIVE_OFF_NELSON: &str = 
+"SOMBRE ROI RENE : OFF :no:  shisOPTI  :Oiso: 
+
+twitch game : **twitch title**
+
+https://www.twitch.tv/+user_id";
+
+pub const LIVE_COMMAND: &str = "!live";
